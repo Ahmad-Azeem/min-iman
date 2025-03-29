ada9afb75dc7cdd25978755ef27a4d7ccebb35da
Azeem <b122310496@student.utem.edu.my>
2025-03-29 23:16:51 +0800

    Testing for inserting a document part 1

--- index2.js
+++ index2.js
@@ -7,12 +7,15 @@ const client = new MongoClient(uri);
 
 try{
     await client.connect();
-    console.log("Connected to MongoDB");
-
+    console.log("Connected to MongoDB");//console.log very important to check if the connection is successful
+     
+    //Access the database and collection
     const db = client.db("testDB");
     const collection =db.collection ("users");
 
     //Insert a document
+    await collection.insertOne({ name: "Syafiq", age: 55});
+    console.log("Document inserted!");
     //await collection.insertOne({ name: "Syafiq", age: 23});
     console.log("Document inserted!");
 
